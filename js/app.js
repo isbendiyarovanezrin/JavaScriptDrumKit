33d86ec6bf4fbeddb1e605d93026d065ec02cbf1
Nəzrin <isbndyrva@gmail.com>
2021-12-27 21:02:19 +0400

    Add mousemove cursor

--- js/app.js
+++ js/app.js
@@ -13,3 +13,14 @@ keys.forEach((key) => key.addEventListener("transitionend", removeStyle));
 function removeStyle() {
   this.classList.remove("playing");
 }
+
+// cursor
+let cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
